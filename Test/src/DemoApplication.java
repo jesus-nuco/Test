7a222d3d9c5c2890887673e9da5240d46d60ba53
jredon <jredon@jredon-HP-ProBook-450-G5>
2018-05-18 16:28:10 -0400

    ddd

---
+++ Test/src/DemoApplication.java
@@ -0,0 +1,50 @@
+
+import org.aion.api.IAionAPI;
+import org.aion.api.type.ApiMsg;
+import org.aion.api.type.BlockDetails;
+
+public class DemoApplication {
+	IAionAPI aionApi = null;
+
+	public static void main(String[] args) {
+		
+
+		 new DemoApplication();
+	}
+
+	public DemoApplication() {
+
+		System.out.println("WWWWWWWWWWWWWWWWWWWWW");
+
+		// 8547 2 aionv2user aionv2pws 0 13
+		
+		// DemoBlockNumber() ;
+		System.out.println("A");
+
+		ApiMsg apiMsg = new ApiMsg();
+		System.out.println("B");
+
+		aionApi = IAionAPI.init();
+		apiMsg = new ApiMsg();
+
+		apiMsg.set(aionApi.connect("tcp://127.0.0.1:8547"));
+		if (apiMsg.isError()) {
+			System.out.println("Aion api connection could not be established.");
+			closeApi();
+		}
+
+		
+		// aionApi.connect(config.getNodeAddress(0));
+		System.out.println("F");
+		System.out.println("WWWWWWWWWWWWWWWWWWWWW:" + aionApi.getAccount().toString());
+		System.out.println("WWWWWWWWWWWWWWWWWWWWW:" + aionApi.getWallet().getMinerAccount().getObject().toString());
+		closeApi();
+	}
+
+	private void closeApi() {
+		if (aionApi != null)
+			aionApi.destroyApi();
+	}
+
+	 
+}
